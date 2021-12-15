d0b9183d9d51b7b41cba11b9cc548eaff7f6475d
simonkorl <machuan0228@sina.com>
2021-12-15 20:23:09 +0800

    feat: move logs to dirs

--- examples/dtp_test/run_test.sh
+++ examples/dtp_test/run_test.sh
@@ -10,10 +10,12 @@ SERVER_PORT=${SERVER_PORT:-5555}
 TRACE_FILE=${TRACE_FILE:-trace/block_trace/aitrans_block.txt}
 TRACE_FILE_BASENAME=${TRACE_FILE##*/}
 TRACE_FILE_BASENAME=${TRACE_FILE_BASENAME%.*}
-TC_LOG=${TC_LOG:-tc.log}
-SERVER_LOG=${SERVER_LOG:-server.log}
+TIME=$(date +"%m%d-%H%M%S")
+DIR_NAME="logs/${TRACE_FILE_BASENAME}-${TIME}"
+TC_LOG="${DIR_NAME}/${TC_LOG:-tc.log}"
+SERVER_LOG="${DIR_NAME}/${SERVER_LOG:-server.log}"
 COUNT=0
-CLIENT_LOG=${CLIENT_LOG:-client}-${TRACE_FILE_BASENAME}
+CLIENT_LOG="${DIR_NAME}/${CLIENT_LOG:-client}-${TRACE_FILE_BASENAME}"
 
 # sudo python /home/ubuntu1/dtp_test_scripts/traffic_control.py --load /home/ubuntu1/dtp_test_scripts/net_trace/traces_17.txt -nic enp1s0 > tc.log 2>&1 & ./server 192.168.0.2 5555 trace/block_trace/aitrans_block.txt
 
@@ -22,17 +24,21 @@ if [ "$1" == "help" ]; then
 
 elif [ "$1" == "server" ]; then
 
+    mkdir -p ./${DIR_NAME}
+
     if [ $TRAFFIC_CONTROL == true ]; then
 
         python ${TRAFFIC_CONTROL_SCRIPT} --load ${NET_TRACE} ${TRAFFIC_CONTROL_ARGS} > ${TC_LOG} 2>&1 & ${SERVER} ${SERVER_IP} ${SERVER_PORT} ${TRACE_FILE} &> ${SERVER_LOG}
     
     else
 
-        {SERVER} ${SERVER_IP} ${SERVER_PORT} ${TRACE_FILE} &> ${SERVER_LOG}
+        ${SERVER} ${SERVER_IP} ${SERVER_PORT} ${TRACE_FILE} &> ${SERVER_LOG}
     
     fi
 
 elif [ "$1" == "client" ]; then
+
+    mkdir -p ./${DIR_NAME}
 
     if [ $TRAFFIC_CONTROL == true ]; then
 
@@ -46,7 +52,7 @@ elif [ "$1" == "client" ]; then
 
         for ((COUNT; COUNT < $2; COUNT++)); do
 
-            {CLIENT} ${SERVER_IP} ${SERVER_PORT} && mv client.log ${CLIENT_LOG}-${COUNT}.log
+            ${CLIENT} ${SERVER_IP} ${SERVER_PORT} && mv client.log ${CLIENT_LOG}-${COUNT}.log
 
         done
     
